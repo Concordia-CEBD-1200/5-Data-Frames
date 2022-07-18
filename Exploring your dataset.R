44910e12b6b6e07bea8e9fb814dadb08526d0c86
Pranav Bhatia <30525912+PranavBhatia@users.noreply.github.com>
2022-07-17 23:08:29 -0400

    Intro to qplot

--- Exploring your dataset.R
+++ Exploring your dataset.R
@@ -66,3 +66,38 @@ stats[stats$Income.Group == "High income",]
 levels(stats$Income.Group)
 
 stats[stats$Country.Name == "Malta",]
+
+# ------Intro to qplot()------
+#install.packages("ggplot2")
+library(ggplot2)
+? qplot
+
+qplot(data = stats, x = Internet.users)
+qplot(data = stats, x = Income.Group, y = Birth.rate)
+qplot(
+  data = stats,
+  x = Income.Group,
+  y = Birth.rate,
+  size = I(3)
+)
+qplot(
+  data = stats,
+  x = Income.Group,
+  y = Birth.rate,
+  size = I(10)
+)
+
+qplot(
+  data = stats,
+  x = Income.Group,
+  y = Birth.rate,
+  size = I(3),
+  color = I("blue")
+)
+
+qplot(
+  data = stats,
+  x = Income.Group,
+  y = Birth.rate,
+  geom="boxplot"
+)
