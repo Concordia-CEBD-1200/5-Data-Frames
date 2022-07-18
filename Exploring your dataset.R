9166f8102d6d4b20cec5c03a8941cd00dbe067d7
Pranav Bhatia <30525912+PranavBhatia@users.noreply.github.com>
2022-07-17 23:14:10 -0400

    Visualizing What We Need With Qplot: Part I

--- Exploring your dataset.R
+++ Exploring your dataset.R
@@ -101,3 +101,21 @@ qplot(
   y = Birth.rate,
   geom="boxplot"
 )
+
+# Visualizing What We Need With Qplot: Part I
+qplot(data = stats, x = Internet.users, y = Birth.rate)
+qplot(
+  data = stats,
+  x = Internet.users,
+  y = Birth.rate,
+  size = I(4),
+  color = I("red")
+)
+
+qplot(
+  data = stats,
+  x = Internet.users,
+  y = Birth.rate,
+  size = I(4),
+  color = Income.Group
+)
