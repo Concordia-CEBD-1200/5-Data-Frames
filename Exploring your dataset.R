51d18cfd380fdac8c875c395f9b8b490e65e1bf2
Pranav Bhatia <30525912+PranavBhatia@users.noreply.github.com>
2022-07-17 23:07:16 -0400

    Formatting

--- Exploring your dataset.R
+++ Exploring your dataset.R
@@ -21,7 +21,7 @@ summary(stats)
 # ------Using the $ sign------
 stats
 head(stats)
-stats[3: 3]
+stats[3:3]
 stats[3, "Birth.rate"]
 stats["Angola", 3] # won't work as row's don't have any name in data frames
 stats$Country.Name
@@ -31,14 +31,14 @@ stats[, "Internet.users"] # same as stats$Internet.users
 levels(as.factor(stats$Income.Group)) # or use stringsAsFactors = T
 
 # ------Basic operations with a Data Frame------
-stats[1:10, ] # subsetting
-stats[3:9, ]
-stats[c(4, 100), ]
+stats[1:10,] # subsetting
+stats[3:9,]
+stats[c(4, 100),]
 # Remember how the [] work?
-stats[1,]
-is.data.frame(stats[1,]) # unlike matrix
-is.data.frame(stats[,1]) # returns a vector
-is.data.frame(stats[,1, drop=F])
+stats[1, ]
+is.data.frame(stats[1, ]) # unlike matrix
+is.data.frame(stats[, 1]) # returns a vector
+is.data.frame(stats[, 1, drop = F])
 # Mathematical
 head(stats)
 stats$Birth.rate * stats$Internet.users
@@ -48,7 +48,7 @@ head(stats)
 stats$MyCalc <- stats$Birth.rate * stats$Internet.users
 # Recycling
 stats$xyz <- 1:5
-head(stats, n=15)
+head(stats, n = 15)
 stats$xyz <- 1:4
 # Remove a column
 head(stats)
@@ -58,11 +58,11 @@ stats$xyz <- NULL
 # ------Filtering a Data Frame------
 head(stats)
 filter <- stats$Internet.users < 2
-stats[filter, ]
+stats[filter,]
 
-stats[stats$Birth.rate > 40, ]
-stats[stats$Birth.rate > 40 & stats$Internet.users < 2, ]
-stats[stats$Income.Group == "High income", ]
+stats[stats$Birth.rate > 40,]
+stats[stats$Birth.rate > 40 & stats$Internet.users < 2,]
+stats[stats$Income.Group == "High income",]
 levels(stats$Income.Group)
 
-stats[stats$Country.Name == "Malta", ]
+stats[stats$Country.Name == "Malta",]
