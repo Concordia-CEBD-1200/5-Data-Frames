836e6d4f3214aeefa7a52b41353b4e1661851546
Pranav Bhatia <30525912+PranavBhatia@users.noreply.github.com>
2022-07-17 22:31:18 -0400

    Using the $ sign

--- Exploring your dataset.R
+++ Exploring your dataset.R
@@ -17,3 +17,27 @@ tail(stats) # Bottom 6 rows
 str(stats)
 
 summary(stats)
+
+# ------Using the $ sign------
+stats
+head(stats)
+stats[3: 3]
+stats[3, "Birth.rate"]
+stats["Angola", 3] # won't work as row's don't have any name in data frames
+stats$Country.Name
+stats$Internet.users
+stats$Internet.users[2]
+stats[, "Internet.users"] # same as stats$Internet.users
+levels(as.factor(stats$Income.Group)) # or use stringsAsFactors = T
+
+
+
+
+
+
+
+
+
+
+
+
