4da0a9014b98321e86d987ea64ca02c2fd7f7b66
Pranav Bhatia <30525912+PranavBhatia@users.noreply.github.com>
2022-07-23 18:25:21 -0400

    Create Building Dataframes.R

---
+++ Building Dataframes.R
@@ -0,0 +1,19 @@
+# CREATING DATAFRAMES
+
+mydf = data.frame(Countries_2012_Dataset,
+                  Codes_2012_Dataset,
+                  Regions_2012_Dataset)
+
+head(mydf)
+colnames(mydf) <- c("Country", "Code", "Region")
+head(mydf)
+
+rm(mydf)
+
+mydf = data.frame(Country = Countries_2012_Dataset,
+                  Code = Codes_2012_Dataset,
+                  Region = Regions_2012_Dataset)
+
+head(mydf)
+tail(mydf)
+summary(mydf)
